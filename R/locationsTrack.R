210b81a56b57f43c0758e152025e87d3b7c1dde0
henningte <34153309+henningte@users.noreply.github.com>
2018-10-05 16:23:04 +0200

    Reformated locationsTrack: Split the function in several sub-functions, updated the output to the new requirements of the workflow, corrected some malfunctions.

--- R/locationsTrack.R
+++ R/locationsTrack.R
@@ -73,34 +73,28 @@ NULL
 #'     \item{location}{An integer value for each identified
 #'     spatial point cluster (location) increasing with the time starting
 #'     from 1.}
-#'     \item{residencetime}{A numerical vector indicating the residence time
-#'     of each visit [s].}
-#'     \item{cmapsite}{A logical value indicating if a visits of a
-#'     location is classified as long-term visit (campsite) (\code{TRUE})
-#'     or as short-term visit (\code{FALSE}).}
-#'     \item{arrivaltime}{A \code{POSIXct} vector indicating the arrival time
-#'     of the visit at the respective location.}
-#'     \item{departuretime}{A \code{POSIXct} vector indicating the departure
-#'     time of the visit from the respective location.}
-#'     \item{arrivaltimeindex}{An integer vector indicating the index of the
-#'     data value of the input \code{\link[trajectories]{Track}} object that
-#'     represents the arrival at the location for the respective visit.}
-#'     \item{departuretimeindex}{An integer vector indicating the index of the
-#'     data value of the input \code{\link[trajectories]{Track}} object that
-#'     represents the departure from the location for the respective visit.}
-#'     \item{\code{visitsloc}}{An integer vector indicating the number of
-#'     visits at a specific location the data point is assigned to (i.e.
-#'     a counter for the visits at a specific location).}
-#'     \item{\code{visitscampsite}}{An integer vector indicating the number
-#'     of visits at a specific location the data point is assigned to that
-#'     are classified as long-term visits (campsites) (i.e.
-#'     a counter for the long-term visits at a specific location).}
 #'     \code{lon}{The longitude of the respective location (as mean value of
 #'     the coordinates of the data values assigned to the visit).}
 #'     \code{lat}{The latitude of the respective location (as mean value of
 #'     the coordinates of the data values assigned to the visit).}
 #'     \code{alt}{The altitude of the respective location (as mean value of
 #'     the coordinates of the data values assigned to the visit).}
+#'     \item{\code{visitsloc}}{An integer vector indicating the number of
+#'     visits at a specific location the data point is assigned to (i.e.
+#'     a counter for the visits at a specific location).}
+#'     \item{campsite}{A logical value indicating if a visits of a
+#'     location is classified as long-term visit (campsite) (\code{TRUE})
+#'     or as short-term visit (\code{FALSE}).}
+#'     \item{\code{visitscampsite}}{An integer vector indicating the number
+#'     of visits at a specific location the data point is assigned to that
+#'     are classified as long-term visits (campsites) (i.e.
+#'     a counter for the long-term visits at a specific location).}
+#'     \item{arrivaltime}{A \code{POSIXct} vector indicating the arrival time
+#'     of the visit at the respective location.}
+#'     \item{departuretime}{A \code{POSIXct} vector indicating the departure
+#'     time of the visit from the respective location.}
+#'     \item{residencetime}{A numerical vector indicating the residence time
+#'     of each visit [s].}
 #'     \code{speed}{The speed of the respective location (as mean value of
 #'     the speed values of the data values assigned to the visit).}
 #'   }
@@ -111,160 +105,12 @@ NULL
 #' \code{\link{extractClustersBuffer}}.
 #' @examples #
 #' @export
-locationsTracks <- function(currenttrack,
-                            radius = 200,
-                            tmin = 345600,
-                            summary = TRUE){
-
-  # convert track to SpatialPointsDataFrame and transform to UTM
-  trsSP <- TrackToSpatialPointsDataFrame(currenttrack)
-
-  # cluster the data points
-  initiallocationids <- extractClustersBuffer(trsSP, radius)
-
-  # add variable to trsSP
-  trsSP$campsites <- initiallocationids
-
-  # return NULL if a data set contains only one location (no Track object can be constructed from one point)
-  if(length(unique(initiallocationids)) == 1){
-    warning("Only one location identified!")
-    return(NULL)
-  }
-
-  # find order of campsite visits
-  ordercampsitevisits <- lapply(unique(initiallocationids)[unique(initiallocationids) != 0], function(x){
-    identifyBlocksVariable(currenttrack = trsSP, variable = "campsites", value = x)}
-  )
-  ordercampsitevisits <- as.matrix(do.call(rbind, ordercampsitevisits))
-  ordercampsitevisits <- ordercampsitevisits[order(ordercampsitevisits[,1]),]
-
-  # create a list with respective indices
-  ordercampsitevisits <- lapply(seq_len(nrow(ordercampsitevisits)), function(x){ordercampsitevisits[x,1]:(ordercampsitevisits[x,2])})
-
-  # define a dataframe to store the results in (with the location id of each visit)
-  campsitesvar <- data.frame(location = trsSP$campsites[sapply(ordercampsitevisits, function(x){x[1]})])
-
-  # duration of each visit
-  campsitesvar$residencetime = sapply(ordercampsitevisits, function(x){difftime(trsSP$time[range(x)[2]], trsSP$time[range(x)[1]], units = "sec")})
-
-  # classify visits as campsites or short term visits
-  campsitesvar$campsite <- rep(T, nrow(campsitesvar))
-  campsitesvar$campsite[which(campsitesvar$residencetime < tmin)] <- FALSE
-
-  # arrival and departure time
-  campsitesvar$arrivaltime <- sapply(ordercampsitevisits, function(x){as.character(trsSP$time[x[1]])})
-  campsitesvar$departuretime <- sapply(ordercampsitevisits, function(x){as.character(trsSP$time[x[length(x)]])})
-
-  # arrival and departure time as row indices
-  campsitesvar$arrivaltimeindex <- sapply(ordercampsitevisits, function(x){range(x)[1]})
-  campsitesvar$departuretimeindex <- sapply(ordercampsitevisits, function(x){range(x)[2]})
-
-  # number of visits per location
-  visits <- rep(NA, nrow(campsitesvar))
-  for(loc_i in unique(campsitesvar$location)){
-
-    visits[which(campsitesvar$location == loc_i)] <- seq(from = 1, to = length(which(campsitesvar$location == loc_i)))
-
-  }
-  campsitesvar$visitsloc <- visits
-
-  # number of visits per campsite
-  visits <- rep(NA, nrow(campsitesvar))
-  for(loc_i in unique(campsitesvar$location)){
-
-    visits[which(campsitesvar$location == loc_i & campsitesvar$campsite == T)] <- seq(from = 1, to = length(which(campsitesvar$location == loc_i & campsitesvar$campsite == TRUE)))
-
-  }
-  campsitesvar$visitscampsite <- visits
-
-  # redefine indices of locations (according to arrival time)
-  campsitesvar <- redefineIndices(df = campsitesvar, indices = "location", time = "arrivaltime", notchange = 0)
-
-  # get mean position data for each location visit
-  centroidcoords <- data.frame()
-  for(visit_i in c(1:length(ordercampsitevisits))){
-
-    centroidcoords <- rbind(centroidcoords, apply(matrix(trsSP@coords[ordercampsitevisits[[visit_i]],], ncol = 2), 2 , mean))
-
-  }
-  centroidcoords$alt <- sapply(ordercampsitevisits, function(x){mean(trsSP$HEIGHT[x])})
-  names(centroidcoords) <- c("lon", "lat", "alt")
-
-  # convert to SP
-  centroidcoords <- SpatialPoints(centroidcoords[,-3], proj4string = CRS(proj4string(trsSP)))
-
-  # transform back to WGS84 longitude latitude
-  a <- spTransform(centroidcoords, currenttrack@sp@proj4string)
-
-  # add variables to campsitesvar
-  campsitesvar$lon <- a@coords[,1]
-  campsitesvar$lat <- a@coords[,2]
-  campsitesvar$alt <- sapply(ordercampsitevisits, function(x){mean(trsSP$HEIGHT[x])})
-
-  # get mean speed at each location visit
-  campsitesvar$speed <- sapply(ordercampsitevisits, function(x){mean(trsSP$SPEED[x])})
-
-  # create SpatialPointsDataFrame from campsitesvar
-  campsitesvar1 <- campsitesvar
-  campsitesvar <- SpatialPoints(coords = data.frame(lon = campsitesvar$lon, lat = campsitesvar$lat), proj4string = CRS(
-    "+proj=longlat +zone=46 +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
-  ))
-  campsitesvar <-
-    spTransform(
-      campsitesvar,
-      CRS(
-        "+proj=utm +zone=46 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
-      )
-    )
-
-  # create Track object from campsitesvar
-  campsitesvar <- Track(STIDF(sp = campsitesvar, time = as.POSIXct(campsitesvar1$arrivaltime) , data = campsitesvar1, endTime = as.POSIXct(campsitesvar1$departuretime)))
-
-  # add information to currenttrack
-  locationid <- rep(NA, nrow(trsSP@data))
-  campsite <- rep(NA, nrow(trsSP@data))
-  visitsloc <- rep(NA, nrow(trsSP@data))
-  visitscampsite <- rep(NA, nrow(trsSP@data))
-  for(visit_i in c(1:nrow(campsitesvar))){
-
-    # get time interval of visit
-    timeint <- c(campsitesvar$arrivaltimeindex[visit_i], campsitesvar$departuretimeindex[visit_i])
-
-    # define condition
-    cond <- seq(timeint[1], timeint[2])
-
-    # add location id to locationid
-    locationid[cond] <- campsitesvar$location[visit_i]
-
-    # add information on campsite to campsite
-    campsite[cond] <- campsitesvar$campsite[visit_i]
-
-    # add information on number of visits to visitsloc
-    visitsloc[cond] <- campsitesvar$visitsloc[visit_i]
-
-    # add information on number of visits to visitscampsite
-    visitscampsite[cond] <- campsitesvar$visitscampsite[visit_i]
-  }
-
-  # add locationid, campsite and visit to currenttrack.df
-  currenttrack@data$location <- locationid
-  currenttrack@data$campsite <- campsite
-  currenttrack@data$visitsloc <- visitsloc
-  currenttrack@data$visitscampsite <- visitscampsite
-
-  # return the result
-  ifelse(summary == TRUE, campsitesvar, currenttrack)
-
-}
-
-#####
-
-locationsTracks <- function(currenttrack,
-                            radius = 200,
-                            tmin = 345600,
-                            tmaxinterstices = 345600,
-                            timeinterval = 30*60,
-                            summary = TRUE){
+locationsTrack <- function(currenttrack,
+                           radius = 200,
+                           tmin = 345600,
+                           tmaxinterstices = 345600,
+                           timeinterval = 30*60,
+                           summary = TRUE){
 
   # convert track to SpatialPointsDataFrame and transform to UTM
   trsSP <- TrackToSpatialPointsDataFrame(currenttrack)
@@ -286,6 +132,7 @@ locationsTracks <- function(currenttrack,
 
     # get the block start and end indices for the current location
     blockindices <- identifyBlocksVariable(currenttrack = trsSP, variable = "location", value = x)
+
     row.names(blockindices) <- rep(x, nrow(blockindices))
     return(blockindices)
 
@@ -315,6 +162,57 @@ locationsTracks <- function(currenttrack,
   # count the number of repeated long-term visits at each location
   trackindicesvisits <- countAllReapeatedLongTermVisits(trackindicesvisits, locations)
 
+  # add locations to trackindicesvisits
+  trackindicesvisits <- cbind(trackindicesvisits, locations)
+
+  # order the entries of trackindicesvisits
+  trackindicesvisits <- trackindicesvisits[order(trackindicesvisits[,1]),]
+
+  # add all values to the input track object
+  tracklocationsvisits <- do.call(rbind, lapply(seq_len(nrow(trackindicesvisits)), function(x){
+
+    do.call(rbind, lapply(trackindicesvisits[x, 1]:trackindicesvisits[x, 2], function(y){
+      trackindicesvisits[x, c(8, 6, 3, 7)]
+    }))
+
+  }))
+  currenttrack$location <- tracklocationsvisits[,1]
+  currenttrack$campsite <- tracklocationsvisits[,2]
+  currenttrack$visitsloc <- tracklocationsvisits[,3]
+  currenttrack$visitscampsite <- tracklocationsvisits[,4]
+
+  # set the values for location, campsite, visitsloc and visitscampsite to NA for gaps
+  currenttrack@data[which(currenttrack$location == 0),(ncol(currenttrack@data)-3):ncol(currenttrack@data)] <- NA
+
+  # summarise the values for each visit and location
+  if(summary == TRUE){
+
+    # define an index for each location and visit
+    indexvisitlocation <- paste0(currenttrack$location, "_", currenttrack$visitsloc)
+
+    # compute mean coordinates for each visit
+    coordinatesvisitlocation <- SpatialPoints(t(sapply(unique(indexvisitlocation), function(x) c(mean(trsSP@coords[indexvisitlocation == x,1]), mean(trsSP@coords[indexvisitlocation == x,2])))), proj4string = trsSP@proj4string)
+    coordinatesvisitlocation <- spTransform(coordinatesvisitlocation, currenttrack@sp@proj4string)@coords
+
+    summaryvisitslocation <- data.frame(
+      location = sapply(unique(indexvisitlocation), function(x) currenttrack$location[indexvisitlocation == x][1]),
+      lon = coordinatesvisitlocation[,1],
+      lat = coordinatesvisitlocation[,2],
+      alt = sapply(unique(indexvisitlocation), function(x) mean(as.numeric(currenttrack$HEIGHT[indexvisitlocation == x]))),
+      visitsloc = sapply(unique(indexvisitlocation), function(x) currenttrack$visitsloc[indexvisitlocation == x][1]),
+      campsite = sapply(unique(indexvisitlocation), function(x) currenttrack$campsite[indexvisitlocation == x][1]),
+      visitscampsite = sapply(unique(indexvisitlocation), function(x) currenttrack$visitscampsite[indexvisitlocation == x][1]),
+      arrivaltime = as.POSIXct(sapply(unique(indexvisitlocation), function(x) currenttrack$time[indexvisitlocation == x][1]), origin = "1970-01-01 00:00:00"),
+      departuretime = as.POSIXct(sapply(unique(indexvisitlocation), function(x) currenttrack$time[indexvisitlocation == x][length(which(indexvisitlocation == x))]), origin = "1970-01-01 00:00:00"),
+      residencetime = sapply(unique(indexvisitlocation), function(x) difftime(currenttrack$time[indexvisitlocation == x][length(which(indexvisitlocation == x))], currenttrack$time[indexvisitlocation == x][1], units = "sec")),
+      speed = sapply(unique(indexvisitlocation), function(x) mean(as.numeric(currenttrack$SPEED[indexvisitlocation == x])))
+        )
+
+    summaryvisitslocation[-is.na(summaryvisitslocation$location),]
+
+  }else{
+    return(currenttrack)
+  }
 
 }
 
