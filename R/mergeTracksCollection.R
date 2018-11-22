7617464132b474ee6029e4b47307c577c6ca0532
henningte <34153309+henningte@users.noreply.github.com>
2018-11-22 10:35:19 +0100

    Corrected the funcizton output.

--- R/mergeTracksCollection.R
+++ R/mergeTracksCollection.R
@@ -31,11 +31,11 @@ mergeTracksCollection <- function(trackscollection, cores = 1, clcall = NULL){
     # set up cluster
     cl <- makeCluster(cores, outfile="", type = "PSOCK")
     registerDoParallel(cl)
+    clusterCall(cl, function(){library("lubridate")})
+    clusterCall(cl, function(){library("spacetime")})
     if(is.null(clcall) == FALSE){
       clusterCall(cl, clcall)
     }
-    clusterCall(cl, function(){library("lubridate")})
-    clusterCall(cl, function(){library("spacetime")})
     clusterExport(cl = cl, varlist = list("exDataTracks", "mergeTracks", "trackscollection"), envir=environment())
 
     # merge the Track objects of each Tracks object
@@ -50,5 +50,8 @@ mergeTracksCollection <- function(trackscollection, cores = 1, clcall = NULL){
 
   # set the Track names to the original names
   names(newtracks@tracks) <- names(trackscollection@tracksCollection)
+
+  # return the result
+  return(newtracks)
 
 }
