ebceb9042603b4cff448d71b7ab19a7b830793da
Christian Knoth <christian.knoth@uni-muenster.de>
2019-03-08 16:11:36 +0100

    added function to compute shortest distance of locations in tracks to lines (such as rivers) or polygon borders (such as district borders)

---
+++ R/min_dist_to_feature.R
@@ -0,0 +1,48 @@
+
+#'@importFrom Rdpack reprompt
+#'@import rgdal
+#'@import geosphere
+#'@import sp
+NULL
+
+#' Computes the minimum distance of locations to the nearest line or polygon border
+#'
+#' \code{min_dist_to_feature} computes the shortest distance of each location (as identified by \code{\link{locationsTracks}}) in a \code{Tracks} object to the closest polyline or polygon border.
+#' @param hh A \code{Tracks} object usually representing movement of one household.
+#' @param feature A SpatialLines or SpatialPolygons object containing the features to compute distance to.
+#' @return A \code{vector} with the shortest distance of all locations in \code{hh} to the closest feature in \code{feature}.
+#' @seealso
+#' @examples #
+#' @export
+min_dist_to_feature <- function(hh, feature, minrange=10000){
+  
+  #extract SpatialPoints of locations
+  locations <- SpatialPoints(unique(hh@tracks[[1]]@sp@coords), proj4string = hh@tracks[[1]]@sp@proj4string)
+  
+  #check CRS of both inputs
+  if(!proj4string(locations)=="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"){
+    locations <- spTransform(locations,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+  }
+  if(!proj4string(feature)=="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"){
+    feature <- spTransform(feature,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+  }
+  
+  distances <- c()
+  #iterate through locations, for each location...
+  for(i in 1:length(locations)){
+    #check whether there are features in the minimum range (specified by minrange)
+    bufferrange <- minrange
+    feat_in_range <- suppressWarnings(intersect(feature,buffer(locations[i], bufferrange)))
+    # if there are none, feat_in_range will be NULL, in this case extend range iteratively by 10000m until     
+    #feat_in_range is not NULL
+    while(is.null(feat_in_range)){
+      bufferrange <- bufferrange+10000
+      feat_in_range <- suppressWarnings(intersect(feature,buffer(locations[i], bufferrange)))
+    }
+    #then calculate shortest distance to features in feat_in_range and add it to the vector of distances for   
+    #all locations
+    distances[i] <- dist2Line(locations[i],feat_in_range)[,1]
+  }
+  print(Sys.time())
+  return(distances)
+}
